72a25e7b0f07ff96617405d4c3b73948151aac23
RevLogi <au5mau6@gmail.com>
2026-03-06 17:24:46 +0800

    Finish lab07

--- fa24-lab-starter/lab07/ex1.c
+++ fa24-lab-starter/lab07/ex1.c
@@ -1,15 +1,17 @@
-#include <time.h>
-#include <stdio.h>
-#include <x86intrin.h>
 #include "ex1.h"
+
+#include <emmintrin.h>
+#include <stdio.h>
+#include <time.h>
+#include <x86intrin.h>
 
 long long int sum(int vals[NUM_ELEMS]) {
     clock_t start = clock();
 
     long long int sum = 0;
-    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        for(unsigned int i = 0; i < NUM_ELEMS; i++) {
-            if(vals[i] >= 128) {
+    for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        for (unsigned int i = 0; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
                 sum += vals[i];
             }
         }
@@ -23,18 +25,18 @@ long long int sum_unrolled(int vals[NUM_ELEMS]) {
     clock_t start = clock();
     long long int sum = 0;
 
-    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
-            if(vals[i] >= 128) sum += vals[i];
-            if(vals[i + 1] >= 128) sum += vals[i + 1];
-            if(vals[i + 2] >= 128) sum += vals[i + 2];
-            if(vals[i + 3] >= 128) sum += vals[i + 3];
+    for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            if (vals[i] >= 128) sum += vals[i];
+            if (vals[i + 1] >= 128) sum += vals[i + 1];
+            if (vals[i + 2] >= 128) sum += vals[i + 2];
+            if (vals[i + 3] >= 128) sum += vals[i + 3];
         }
 
         // TAIL CASE, for when NUM_ELEMS isn't a multiple of 4
         // NUM_ELEMS / 4 * 4 is the largest multiple of 4 less than NUM_ELEMS
         // Order is important, since (NUM_ELEMS / 4) effectively rounds down first
-        for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+        for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
             if (vals[i] >= 128) {
                 sum += vals[i];
             }
@@ -47,14 +49,33 @@ long long int sum_unrolled(int vals[NUM_ELEMS]) {
 
 long long int sum_simd(int vals[NUM_ELEMS]) {
     clock_t start = clock();
-    __m128i _127 = _mm_set1_epi32(127); // This is a vector with 127s in it... Why might you need this?
-    long long int result = 0; // This is where you should put your final result!
+    __m128i _127 =
+        _mm_set1_epi32(127);   // This is a vector with 127s in it... Why might you need this?
+    long long int result = 0;  // This is where you should put your final result!
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
-    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+    for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
+        __m128i sum_vec = _mm_setzero_si128();
+        __m128i _vals, _comp;
+
+        for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            _vals = _mm_loadu_si128((__m128i *)&vals[i]);
+            _comp = _mm_cmpgt_epi32(_vals, _127);
+            _vals = _mm_and_si128(_vals, _comp);
+            sum_vec = _mm_add_epi32(sum_vec, _vals);
+        }
+        int tmp_arr[4];
+        _mm_storeu_si128((__m128i *)tmp_arr, sum_vec);
+        result += (long long int)tmp_arr[0] + (long long int)tmp_arr[1] +
+                  (long long int)tmp_arr[2] + (long long int)tmp_arr[3];
 
         /* Hint: you'll need a tail case. */
+        for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
@@ -69,11 +90,52 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     long long int result = 0;
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
-    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+    for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
         /* Copy your sum_simd() implementation here, and unroll it */
+        __m128i sum_vec = _mm_setzero_si128();
+        __m128i _vals, _comp;
+
+        for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+            _vals = _mm_loadu_si128((__m128i *)&vals[i]);
+            _comp = _mm_cmpgt_epi32(_vals, _127);
+            _vals = _mm_and_si128(_vals, _comp);
+            sum_vec = _mm_add_epi32(sum_vec, _vals);
+
+            _vals = _mm_loadu_si128((__m128i *)&vals[i + 4]);
+            _comp = _mm_cmpgt_epi32(_vals, _127);
+            _vals = _mm_and_si128(_vals, _comp);
+            sum_vec = _mm_add_epi32(sum_vec, _vals);
+
+            _vals = _mm_loadu_si128((__m128i *)&vals[i + 8]);
+            _comp = _mm_cmpgt_epi32(_vals, _127);
+            _vals = _mm_and_si128(_vals, _comp);
+            sum_vec = _mm_add_epi32(sum_vec, _vals);
+
+            _vals = _mm_loadu_si128((__m128i *)&vals[i + 12]);
+            _comp = _mm_cmpgt_epi32(_vals, _127);
+            _vals = _mm_and_si128(_vals, _comp);
+            sum_vec = _mm_add_epi32(sum_vec, _vals);
+        }
 
         /* Hint: you'll need 1 or maybe 2 tail cases here. */
+        for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS / 4 * 4; i += 4) {
+            _vals = _mm_loadu_si128((__m128i *)&vals[i]);
+            _comp = _mm_cmpgt_epi32(_vals, _127);
+            _vals = _mm_and_si128(_vals, _comp);
+            sum_vec = _mm_add_epi32(sum_vec, _vals);
+        }
+
+        int tmp_arr[4];
+        _mm_storeu_si128((__m128i *)tmp_arr, sum_vec);
+        result += (long long int)tmp_arr[0] + (long long int)tmp_arr[1] +
+                  (long long int)tmp_arr[2] + (long long int)tmp_arr[3];
+
+        for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
--- fa24-lab-starter/lab07/ex2.c
+++ fa24-lab-starter/lab07/ex2.c
@@ -1,32 +1,55 @@
 #include "ex2.h"
 
-double dotp_naive(double* x, double* y, int arr_size) {
+double dotp_naive(double *x, double *y, int arr_size) {
     double global_sum = 0.0;
-    for (int i = 0; i < arr_size; i++)
-        global_sum += x[i] * y[i];
+    for (int i = 0; i < arr_size; i++) global_sum += x[i] * y[i];
     return global_sum;
 }
 
 // Critical Keyword
-double dotp_critical(double* x, double* y, int arr_size) {
+double dotp_critical(double *x, double *y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
     // Use the critical keyword here!
+
+    #pragma omp parallel for
+    for (int i = 0; i < arr_size; i++) {
+        double temp = x[i] * y[i];
+        #pragma omp critical
+        {
+            global_sum += temp;
+        }
+    }
+
     return global_sum;
 }
 
 // Reduction Keyword
-double dotp_reduction(double* x, double* y, int arr_size) {
+double dotp_reduction(double *x, double *y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
     // Use the reduction keyword here!
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
 // Manual Reduction
-double dotp_manual_reduction(double* x, double* y, int arr_size) {
+double dotp_manual_reduction(double *x, double *y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
+    #pragma omp parallel
+    {
+        double local_sum = 0.0;
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++) {
+            local_sum += x[i] * y[i];
+        }
+
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
+    }
     return global_sum;
 }
